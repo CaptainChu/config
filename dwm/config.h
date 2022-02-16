8ee794d6a4fd52d285f1badc66ea34f961b8809d
Captain Chu <capuchi-tan@gmail.com>
2022-02-16 15:39:21 +0700

    cleaning up

--- dwm/config.h
+++ dwm/config.h
@@ -60,13 +60,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-l", "20","-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *qute[]     = { "qutebrowser", NULL };
 static const char *screenshot[]={ "flameshot", "gui", NULL };
+static const char *discord[]  = { "discord", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask, 		    XK_s, 	   spawn, 		   {.v = screenshot } },
+	{ MODKEY|ShiftMask, 		    XK_c, 	   spawn, 		   {.v = qute } },
+	{ MODKEY|ShiftMask, 		    XK_d, 	   spawn, 		   {.v = discord } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
